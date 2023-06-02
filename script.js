99345c5dd84b40656a5a5f42172d4ed756f6514c
Margaret <124msalvani@frhsd.com>
2023-06-02 09:28:31 -0400

    Everything kinda styled before the inputs

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ let pokemonStatChart = new Chart(myChart,{
     data: {
         labels:["HP", "Attack","Defense", "Special Attack", "Special Defense", "Speed", ],
         datasets:[{
-            label:"Input 'Mon",
+            label:"Input Mon",
             data:[
                 80, 82, 83, 100, 100, 80
             ],
-            backgroundColor:["rgba(88, 144, 11, 0.8)", "grey", "skyblue", "lavender", "rgba(255,99,132,0.6)", "rgba(203, 135, 223, 0.29)", "rgba(33, 237, 103, 0.29)"],
+            backgroundColor:["rgba(101, 173, 226, 0.5)", "navy"],
             borderColor:"black",
             borderWidth:"1",
             hoverBorderColor:"grey",
@@ -21,35 +21,26 @@ let pokemonStatChart = new Chart(myChart,{
         }, {
             label:"Arceus",
             data:[
-                78, 84, 78, 109, 85, 100
+                120, 120, 120, 120, 120, 120
             ],
             // backgroundColor:["grey", pattern.draw('square', '#1f77b4'), "skyblue", "rgba(255, 231, 151, 0.8)", "rgba(18, 136, 116, 0.8)", 'rgba(255, 128, 7, 0.8)'],
             backgroundColor: [
-                pattern.draw('square', '#1f77b4'),
+                "grey",
+                pattern.draw('diamond', 'rgba(213, 192, 31, 0.3)'),
+                /*pattern.draw('square', '#1f77b4'),
                 pattern.draw('circle', '#ff7f0e'),
                 pattern.draw('diamond', '#2ca02c'),
                 pattern.draw('zigzag-horizontal', '#17becf'),
                 pattern.draw('zigzag-horizontal', '#17becf'),
-                pattern.draw('triangle', 'rgb(255, 99, 132, 0.4)') // with opacity
+                pattern.draw('triangle', 'rgb(255, 99, 132, 0.4)') */ // with opacity
               ],
             borderColor:"black",
             borderWidth:"1",
         }]
     },
-    options:{
+    options: {
         legend: {
-            position: "bottom"
-          }
-    },
-    
-
+          position: 'bottom'
+        }
+      }
 }); 
-/*
-const actions = [
-    {name: 'Position: bottom',
-    handler(chart) {
-      chart.options.plugins.legend.position = 'bottom';
-      chart.update();
-    }
-}
-]; */
